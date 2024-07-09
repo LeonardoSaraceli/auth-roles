ebccf2a7d40bdd7796aa675833e69e41be3db77d
Leonardo Lodi <your_email@address>
2024-07-09 20:55:39 +0100

    add authorization and authentication

--- src/domains/user.js
+++ src/domains/user.js
@@ -1,13 +1,32 @@
-const prisma = require('../utils/prisma')
-const bcrypt = require('bcrypt')
+const prisma = require("../utils/prisma")
+const bcrypt = require("bcrypt")
 
-const createUserDb = async (username, password) => await prisma.user.create({
-  data: {
+const createUserDb = async (username, password, role) => {
+  const data = {
     username,
-    passwordHash: await bcrypt.hash(password, 6)
+    passwordHash: await bcrypt.hash(password, 6),
   }
-})
+
+  if (role) {
+    data.role = role
+  }
+
+  const user = await prisma.user.create({ data })
+  delete user.passwordHash
+  return user
+}
+
+const getAllUsersDb = async () => await prisma.user.findMany()
+
+const deleteUserDb = async (id) =>
+  await prisma.user.delete({
+    where: {
+      id: id,
+    },
+  })
 
 module.exports = {
-  createUserDb
+  createUserDb,
+  getAllUsersDb,
+  deleteUserDb,
 }
